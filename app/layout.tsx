831e8deed3915b5c82b04dddd59e27bd1bcdc15a
UsaidMalik <umalik2003@gmail.com>
2024-03-02 15:14:00 -0500

    added in favicon aswell as metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,13 @@ import { menu } from "./menuRoutes"
 import Link from "next/link";
 import ShoppingCart from "./shoppingCart"
 import './globalicons.css'
-
+import type { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: 'Kord Keyboards',
+  description: 'kord keyboards',
+}
 
 export default function RootLayout({
   children,
@@ -15,27 +19,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
       <body className={inter.className}>
         <header>
+          <div>
         <Link href="/">
-          <img src="favicon.ico"/>
+          <img src="Kord Logo White.png" className="h-50px" />
         </Link>
+        </div>
+        <div>
           <nav>
             <ul>
               
             {Object.entries(menu).map(([label, path]) => (
-                <li key={label}>
+                <li key={label} className="text-xl">
                   <Link href={path}>
                     {label}
                   </Link>
                 </li>
               ))}
-
             </ul>
           </nav>
-
+        </div>
+        
+        <div>
         <ShoppingCart/> 
+        </div>
         </header> 
         {children}     
         <footer>
