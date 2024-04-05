7613e4ec480e49cbe266babd76f63d6fcbbdba89
UsaidMalik <umalik2003@gmail.com>
2024-04-05 05:41:59 -0400

    fixed wrapping of suspense

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,23 +3,28 @@ import { useSearchParams } from 'next/navigation'
 import {Suspense} from 'react'
 
 export default  function Page() {
+  return (
+    <div className='py-10'>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
+    </div>
+  )
+}
 
+function Content() {
   const searchParams = useSearchParams();
   const canceled = searchParams.get('status');
 
   return (
-    <div className='py-10'>
-      <Suspense fallback={<div>Loading...</div>}>
-    {canceled == "true" && 
+    canceled == "true" ? 
     <div className="text-center items-center text-black">
         <p className='font-bold text-5xl mb-10'>You're All Set!</p>
         <p className='text-sm'>We'll send you can email with tracking information once your Order's Ready </p>
-    </div> || !canceled && 
-     <div className="text-center items-center text-black">
+    </div> : 
+    <div className="text-center items-center text-black">
      <p className='font-bold text-5xl mb-10'>Your Cart Is Empty!</p>
      <p className='text-sm'>Keep shopping around until you find something you love</p>
- </div> }
- </Suspense>
-  </div>
+    </div> 
   )
 }
