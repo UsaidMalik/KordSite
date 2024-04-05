3cff76829b7cad52a702e91d7164230385a78716
UsaidMalik <umalik2003@gmail.com>
2024-04-05 02:48:47 -0400

    added in routes for canceling and stuff

--- app/checkout/api/route.ts
+++ app/checkout/api/route.ts
@@ -19,14 +19,14 @@ export async function POST(
 
       session = await stripe.checkout.sessions.create({
         line_items: lineItems,
-        shipping_address_collection: "required",
-        allowed_countries: ["US"],
-        shipping_options: ["shr_1P263UIajtiRYLJ0W32ckDBh"],
+        shipping_address_collection: {
+          allowed_countries: ["US"],
+        },
         mode: 'payment',
         success_url: `${requestHeaders.get('origin')}/checkout?status=true`,
         cancel_url: `${requestHeaders.get('origin')}/`,
       });
-      console.log(session.url)
+
     } catch (err) {
       console.log(err)
          return new Response('Generic Server Error', {
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -12,7 +12,11 @@ export default  function Page() {
     <div className="text-center items-center text-black">
         <p className='font-bold text-5xl mb-10'>You're All Set!</p>
         <p className='text-sm'>We'll send you can email with tracking information once your Order's Ready </p>
-    </div>}
+    </div> || !canceled && 
+     <div className="text-center items-center text-black">
+     <p className='font-bold text-5xl mb-10'>Your Cart Is Empty!</p>
+     <p className='text-sm'>Keep shopping around until you find something you love</p>
+ </div> }
   </div>
   )
 }
